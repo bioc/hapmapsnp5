78437f95243df7a87c96d388e7c1757f99641fb7
Benilton Carvalho <bcarvalh@jhsph.edu>
2008-01-18 18:46:58 +0000

    Added SNP 5.0 and SNP 6.0 data for oligo tests
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/hapmapsnp5@543 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+.First.lib <- function(libname, pkgname) {
+  message(paste("/-------------------------------------------\\",
+                "|         SAMPLE HAPMAP SNP 5.0             |",
+                "|-------------------------------------------|",
+                "| Data obtained from http://www.hapmap.org  |",
+                "| This package is meant to be used only for |",
+                "| demonstration of BioConductor packages.   |",
+                "| Access http://www.hapmap.org for details. |",
+                "|-------------------------------------------|",
+                "| The contents of this package are provided |",
+                "| in good faith and the maintainer does not |",
+                "| warrant their accuracy.                   |",
+                "\\-------------------------------------------/",
+                sep="\n"))
+}
